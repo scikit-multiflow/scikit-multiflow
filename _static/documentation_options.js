c588af31fcb2c4ed7b4b19c18c587572c2c5809e
Jacob Montiel <jacob.montiel@gmail.com>
2018-08-21 14:37:42 +0200

    Generated gh-pages for commit 3bac7c8 Author: Jacob Montiel <jacob.montiel@gmail.com>
        Minor update to MissingValuesCleaner

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '0.1.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
