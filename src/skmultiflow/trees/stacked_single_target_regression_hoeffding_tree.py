Fixing floating point numerical instability when normalizing samples

src/skmultiflow/trees/stacked_single_target_regression_hoeffding_tree.py
@@ -578,10 +578,11 @@ class StackedSingleTargetRegressionHoeffdingTree(
                     normalized_meta_prediction = perceptron_weights[1] @ \
                         np.append(normalized_base_prediction, 1.0)
                     mean = self.sum_of_values / self.examples_seen
-                    sd = np.sqrt((self.sum_of_squares -
-                                 (self.sum_of_values ** 2) /
-                                 self.examples_seen) /
-                                 (self.examples_seen - 1))
+                    variance = (self.sum_of_squares -
+                                (self.sum_of_values ** 2) /
+                                self.examples_seen) / (self.examples_seen - 1)
+                    sd = np.sqrt(variance, out=np.zeros_like(variance),
+                                 where=variance >= 0.0)
                     # Samples are normalized using just one sd, as proposed in
                     # the iSoup-Tree method
                     predictions[i] = normalized_meta_prediction * sd + mean
@@ -603,10 +604,11 @@ class StackedSingleTargetRegressionHoeffdingTree(
                         np.append(normalized_base_prediction, 1.0)
 
                     mean = self.sum_of_values / self.examples_seen
-                    sd = np.sqrt((self.sum_of_squares -
-                                 (self.sum_of_values ** 2) /
-                                 self.examples_seen) /
-                                 (self.examples_seen - 1))
+                    variance = (self.sum_of_squares -
+                                (self.sum_of_values ** 2) /
+                                self.examples_seen) / (self.examples_seen - 1)
+                    sd = np.sqrt(variance, out=np.zeros_like(variance),
+                                 where=variance >= 0.0)
 
                     pred_P = normalized_base_prediction * sd + mean
                     pred_SP = normalized_meta_prediction * sd + mean