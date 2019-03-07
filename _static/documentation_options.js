a5194f2936dd31a2d595866530fd9651a08b4cf7
Jacob Montiel <jacob.montiel@gmail.com>
2019-03-07 11:13:23 +0100

    Generated gh-pages for commit 789b3d4 Author: Jacob Montiel <jacob.montiel@gmail.com>
        Increment minor version number

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '0.1.0',
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '0.1.3',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
