32a12207d5b0dfd3073a56b58ef6ac26c50c8ef0
Rémi <remi.adon@gmail.com>
2019-05-15 13:31:59 +0200

    test Source.from_generator can poduce a streaming dataframe

--- tests/data/test_stream.py
+++ tests/data/test_stream.py
@@ -14,3 +14,17 @@ def test_sink_to_list():
     pull_from_stream_df = pd.concat(sub_df_list)
 
     pd.testing.assert_frame_equal(pull_from_stream_df, df)
+
+
+def test_to_dataframe():
+    df = pd.DataFrame(pd.np.random.randint(0, 100,size=(300, 4)), columns=list('ABCD'))
+    gen = DataGenerator(df)
+    stream = streamz.Source.from_generator(gen, batch_size=20, poll_interval=.01)
+    sdf = stream.to_dataframe(example=df.head(1))
+    list_of_df = sdf.stream.sink_to_list()
+    stream.start()
+    time.sleep(1.0)
+
+    df_from_stream = pd.concat(list_of_df, axis=0)
+
+    pd.testing.assert_frame_equal(df_from_stream, df)
