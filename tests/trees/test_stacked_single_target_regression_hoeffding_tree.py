8b2aae3af8679d6db1e753be21e9acea01159208
smastelini <saulomastelini@gmail.com>
2019-07-31 14:09:24 -0300

    Tests for SST-HT added

---
+++ tests/trees/test_stacked_single_target_regression_hoeffding_tree.py
@@ -0,0 +1,130 @@
+import os
+import numpy as np
+from sklearn.metrics import mean_absolute_error
+from skmultiflow.data import RegressionGenerator
+from skmultiflow.trees import StackedSingleTargetRegressionHoeffdingTree
+
+
+def test_stacked_single_target_regression_hoeffding_tree_perceptron(test_path):
+    stream = RegressionGenerator(n_samples=500, n_features=20,
+                                 n_informative=15, random_state=1,
+                                 n_targets=3)
+    stream.prepare_for_use()
+
+    learner = StackedSingleTargetRegressionHoeffdingTree(
+        leaf_prediction='perceptron',
+        random_state=1
+    )
+
+    cnt = 0
+    max_samples = 500
+    wait_samples = 10
+    y_pred = np.zeros((int(max_samples / wait_samples), 3))
+    y_true = np.zeros((int(max_samples / wait_samples), 3))
+
+    while cnt < max_samples:
+        X, y = stream.next_sample()
+        # Test every n samples
+        if (cnt % wait_samples == 0) and (cnt != 0):
+            y_pred[int(cnt / wait_samples), :] = learner.predict(X)
+            y_true[int(cnt / wait_samples), :] = y
+        learner.partial_fit(X, y)
+        cnt += 1
+
+    test_file = os.path.join(
+        test_path,
+        'expected_preds_stacked_single_target_hoeffding_tree_perceptron.npy'
+    )
+
+    expected_predictions = np.load(test_file)
+    assert np.allclose(y_pred, expected_predictions)
+    error = mean_absolute_error(y_true, y_pred)
+    expected_error = 145.96135900202685
+    assert np.isclose(error, expected_error)
+
+    expected_info = "StackedSingleTargetRegressionHoeffdingTree(binary_split=False, grace_period=200,\n" \
+                    "                                           leaf_prediction='perceptron',\n" \
+                    "                                           learning_ratio_const=True,\n" \
+                    "                                           learning_ratio_decay=0.001,\n" \
+                    "                                           learning_ratio_perceptron=0.02,\n" \
+                    "                                           max_byte_size=33554432,\n" \
+                    "                                           memory_estimate_period=1000000,\n" \
+                    "                                           nb_threshold=0, no_preprune=False,\n" \
+                    "                                           nominal_attributes=None,\n" \
+                    "                                           random_state=1,\n" \
+                    "                                           remove_poor_atts=False,\n" \
+                    "                                           split_confidence=1e-07,\n" \
+                    "                                           stop_mem_management=False,\n" \
+                    "                                           tie_threshold=0.05)"
+    assert learner.get_info() == expected_info
+    assert isinstance(learner.get_model_description(), type(''))
+
+
+def test_stacked_single_target_regression_hoeffding_tree_adaptive(test_path):
+    stream = RegressionGenerator(n_samples=500, n_features=20,
+                                 n_informative=15, random_state=1,
+                                 n_targets=3)
+    stream.prepare_for_use()
+
+    learner = StackedSingleTargetRegressionHoeffdingTree(
+        leaf_prediction='adaptive',
+        random_state=1
+    )
+
+    cnt = 0
+    max_samples = 500
+    wait_samples = 10
+    y_pred = np.zeros((int(max_samples / wait_samples), 3))
+    y_true = np.zeros((int(max_samples / wait_samples), 3))
+
+    while cnt < max_samples:
+        X, y = stream.next_sample()
+        # Test every n samples
+        if (cnt % wait_samples == 0) and (cnt != 0):
+            y_pred[int(cnt / wait_samples), :] = learner.predict(X)
+            y_true[int(cnt / wait_samples), :] = y
+        learner.partial_fit(X, y)
+        cnt += 1
+
+    test_file = os.path.join(
+        test_path,
+        'expected_preds_stacked_single_target_hoeffding_tree_adaptive.npy'
+    )
+    expected_predictions = np.load(test_file)
+
+    assert np.allclose(y_pred, expected_predictions)
+    error = mean_absolute_error(y_true, y_pred)
+    expected_error = 134.40951845901694
+    assert np.isclose(error, expected_error)
+
+    expected_info = "StackedSingleTargetRegressionHoeffdingTree(binary_split=False, grace_period=200,\n" \
+                    "                                           leaf_prediction='adaptive',\n" \
+                    "                                           learning_ratio_const=True,\n" \
+                    "                                           learning_ratio_decay=0.001,\n" \
+                    "                                           learning_ratio_perceptron=0.02,\n" \
+                    "                                           max_byte_size=33554432,\n" \
+                    "                                           memory_estimate_period=1000000,\n" \
+                    "                                           nb_threshold=0, no_preprune=False,\n" \
+                    "                                           nominal_attributes=None,\n" \
+                    "                                           random_state=1,\n" \
+                    "                                           remove_poor_atts=False,\n" \
+                    "                                           split_confidence=1e-07,\n" \
+                    "                                           stop_mem_management=False,\n" \
+                    "                                           tie_threshold=0.05)"
+
+    assert learner.get_info() == expected_info
+    assert isinstance(learner.get_model_description(), type(''))
+
+
+def test_hoeffding_tree_coverage(test_path):
+    # Cover nominal attribute observer
+    test_file = os.path.join(test_path, 'multi_target_regression_data.npz')
+    data = np.load(test_file)
+    X = data['X']
+    Y = data['Y']
+
+    learner = StackedSingleTargetRegressionHoeffdingTree(
+        leaf_prediction='mean',
+        nominal_attributes=[i for i in range(3)]
+    )
+    learner.partial_fit(X, Y)
