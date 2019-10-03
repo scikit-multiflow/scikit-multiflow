8d614457e4374b656258302d90e7ec50e235f745
Alban de Crevoisier <albandecrevoisier@gmail.com>
2019-10-03 12:34:22 +0200

    Rename AdaptiveRandomForest in AdaptiveRandomForestClassifier in tests.

--- tests/meta/test_adaptive_random_forest_classifier.py
+++ tests/meta/test_adaptive_random_forest_classifier.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 from skmultiflow.data import RandomTreeGenerator
-from skmultiflow.meta.adaptive_random_forests import AdaptiveRandomForest
+from skmultiflow.meta import AdaptiveRandomForestClassifier
 
 
-def test_adaptive_random_forests():
+def test_adaptive_random_forest_classifier():
     stream = RandomTreeGenerator(tree_random_state=112, sample_random_state=112, n_classes=2)
     stream.prepare_for_use()
 
-    learner = AdaptiveRandomForest(n_estimators=3,
+    learner = AdaptiveRandomForestClassifier(n_estimators=3,
                                    random_state=112)
 
     X, y = stream.next_sample(150)
@@ -44,17 +44,19 @@ def test_adaptive_random_forests():
     assert type(learner.predict(X)) == np.ndarray
     assert np.alltrue(predictions == last_version_predictions)
 
-    expected_info = "AdaptiveRandomForest(binary_split=False, disable_weighted_vote=False,\n" \
-                    "                     drift_detection_method=ADWIN(delta=0.001), grace_period=50,\n" \
-                    "                     lambda_value=6, leaf_prediction='nba',\n" \
-                    "                     max_byte_size=33554432, max_features=5,\n" \
-                    "                     memory_estimate_period=2000000, n_estimators=3,\n" \
-                    "                     nb_threshold=0, no_preprune=False, nominal_attributes=None,\n" \
-                    "                     performance_metric='acc', random_state=112,\n" \
-                    "                     remove_poor_atts=False, split_confidence=0.01,\n" \
-                    "                     split_criterion='info_gain', stop_mem_management=False,\n" \
-                    "                     tie_threshold=0.05,\n" \
-                    "                     warning_detection_method=ADWIN(delta=0.01))"
+    expected_info = "AdaptiveRandomForestClassifier(binary_split=False, disable_weighted_vote=False,\n" \
+                    "                               drift_detection_method=ADWIN(delta=0.001),\n" \
+                    "                               grace_period=50, lambda_value=6,\n" \
+                    "                               leaf_prediction='nba', max_byte_size=33554432,\n" \
+                    "                               max_features=5, memory_estimate_period=2000000,\n" \
+                    "                               n_estimators=3, nb_threshold=0,\n" \
+                    "                               no_preprune=False, nominal_attributes=None,\n" \
+                    "                               performance_metric='acc', random_state=112,\n" \
+                    "                               remove_poor_atts=False, split_confidence=0.01,\n" \
+                    "                               split_criterion='info_gain',\n" \
+                    "                               stop_mem_management=False, tie_threshold=0.05,\n" \
+                    "                               warning_detection_method=ADWIN(delta=0.01))"
+    print(learner.get_info())
     assert learner.get_info() == expected_info
 
 
@@ -62,7 +64,7 @@ def test_adaptive_random_forests_labels_given():
     stream = RandomTreeGenerator(tree_random_state=112, sample_random_state=112, n_classes=2)
     stream.prepare_for_use()
 
-    learner = AdaptiveRandomForest(n_estimators=3,
+    learner = AdaptiveRandomForestClassifier(n_estimators=3,
                                    random_state=112)
 
     X, y = stream.next_sample(150)
@@ -103,7 +105,7 @@ def test_adaptive_random_forests_batch_predict_proba():
     stream = RandomTreeGenerator(tree_random_state=112, sample_random_state=112, n_classes=2)
     stream.prepare_for_use()
 
-    learner = AdaptiveRandomForest(n_estimators=3,
+    learner = AdaptiveRandomForestClassifier(n_estimators=3,
                                    random_state=112)
 
     X, y = stream.next_sample(150)
