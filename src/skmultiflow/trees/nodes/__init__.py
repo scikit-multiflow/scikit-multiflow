4e0f9303ef398344d06f568c7479497d16543eb6
smastelini <mastelini@usp.br>
2019-10-25 18:53:14 -0300

    HATT refactored

--- src/skmultiflow/trees/nodes/__init__.py
+++ src/skmultiflow/trees/nodes/__init__.py
@@ -17,9 +17,16 @@ from .arf_learning_node_nb_adaptive import ARFLearningNodeNBAdaptive
 from .ada_node import AdaNode
 from .ada_split_node import AdaSplitNode
 from .ada_learning_node import AdaLearningNode
+from .anytime_split_node import AnyTimeSplitNode
+from .anytime_active_learning_node import AnyTimeActiveLearningNode
+from .anytime_inactive_learning_node import AnyTimeInactiveLearningNode
+from .anytime_learning_node_nb import AnyTimeLearningNodeNB
+from .anytime_learning_node_nb_adaptive import AnyTimeLearningNodeNBAdaptive
 
 
 __all__ = ["FoundNode", "Node", "SplitNode", "LearningNode", "ActiveLearningNode",
            "InactiveLearningNode", "LearningNodeNB", "LearningNodeNBAdaptive",
            "ARFLearningNode", "ARFLearningNodeNB", "ARFLearningNodeNBAdaptive",
-           "AdaNode", "AdaSplitNode", "AdaLearningNode"]
+           "AdaNode", "AdaSplitNode", "AdaLearningNode", "AnyTimeSplitNode",
+           "AnyTimeActiveLearningNode", "AnyTimeInactiveLearningNode",
+           "AnyTimeLearningNodeNB", "AnyTimeLearningNodeNBAdaptive"]
