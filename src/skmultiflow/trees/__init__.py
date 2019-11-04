f3fb6c26ef2c38c9f8f422aa376ffa7a3ea2f0df
smastelini <mastelini@usp.br>
2019-11-04 13:44:31 -0300

    Adding new added submodules to __init__.py

--- src/skmultiflow/trees/__init__.py
+++ src/skmultiflow/trees/__init__.py
@@ -2,6 +2,10 @@
 The :mod:`skmultiflow.trees` module includes learning methods based on trees.
 """
 
+from . import attribute_observer
+from . import attribute_test
+from . import nodes
+from . import split_criterion
 from .hoeffding_tree import HoeffdingTree
 from .hoeffding_adaptive_tree import HAT
 from .hoeffding_anytime_tree import HATT
@@ -11,6 +15,7 @@ from .regression_hoeffding_adaptive_tree import RegressionHAT
 from .multi_target_regression_hoeffding_tree import MultiTargetRegressionHoeffdingTree
 from .stacked_single_target_hoeffding_tree_regressor import StackedSingleTargetHoeffdingTreeRegressor
 
-__all__ = ["HoeffdingTree", "HAT", "HATT", "LCHT", "RegressionHoeffdingTree",
+__all__ = ["attribute_observer", "attribute_test", "nodes", "split_criterion",
+           "HoeffdingTree", "HAT", "HATT", "LCHT", "RegressionHoeffdingTree",
            "RegressionHAT", "MultiTargetRegressionHoeffdingTree",
            "StackedSingleTargetHoeffdingTreeRegressor"]
