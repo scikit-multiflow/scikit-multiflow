b3209e1b2f87a4330eddf5bc9f2b61c847011ae7
Joze Rozanec <jmrozanec@gmail.com>
2019-12-20 14:43:54 +0100

    Add minmax scaler

---
+++ src/skmultiflow/transform/minmax_scaler.py
@@ -0,0 +1,113 @@
+import numpy as np
+
+from skmultiflow.transform.base_transform import StreamTransform
+from skmultiflow.utils import FastBuffer, get_dimensions
+
+
+class MinmaxScaler(StreamTransform):
+    """ Standardize features by removing the mean and scaling to unit variance.
+    Mean and stdev are computed in given window frame.
+
+    Parameters
+    ----------
+    window_size: int (Default: 200)
+        Defines the window size to compute mean and standard deviation.
+
+    Examples
+    --------
+    """
+
+    def __init__(self, window_size=200):
+        super().__init__()
+        self.window_size = window_size
+        self.window = None
+
+        self.__configure()
+
+    def __configure(self):
+        self.window = FastBuffer(max_size=self.window_size)
+
+    def transform(self, X):
+        """ transform
+
+        Does the transformation process in the samples in X.
+
+        Parameters
+        ----------
+        X: numpy.ndarray of shape (n_samples, n_features)
+            The sample or set of samples that should be transformed.
+
+        """
+        r, c = get_dimensions(X)
+        for i in range(r):
+            row = np.copy([X[i][:]])
+            for j in range(c):
+                value = X[i][j]
+                min = self._get_min(j)
+                max = self._get_max(j)
+                if((max-min)==0):
+                    transformed=0
+                else:
+                    X_std = (value - min) / (max - min)
+                    transformed = X_std * (max - min) + min
+                X[i][j] = transformed
+            self.window.add_element(row)
+        return X
+
+    def _get_min(self, column_index):
+        min = 0.
+        if not self.window.is_empty():
+            min = np.nanmin(np.array(self.window.get_queue())[:, column_index])
+        return min
+
+    def _get_max(self, column_index):
+        max = 1.
+        if not self.window.is_empty():
+            max = np.nanmax(np.array(self.window.get_queue())[:, column_index])
+        return max
+
+    def partial_fit_transform(self, X, y=None):
+        """ partial_fit_transform
+
+        Partially fits the model and then apply the transform to the data.
+
+        Parameters
+        ----------
+        X: numpy.ndarray of shape (n_samples, n_features)
+            The sample or set of samples that should be transformed.
+
+        y: Array-like
+            The true labels.
+
+        Returns
+        -------
+        numpy.ndarray of shape (n_samples, n_features)
+            The transformed data.
+
+        """
+        X = self.transform(X)
+
+        return X
+
+    def partial_fit(self, X, y=None):
+        """ partial_fit
+
+        Partial fits the model.
+
+        Parameters
+        ----------
+        X: numpy.ndarray of shape (n_samples, n_features)
+            The sample or set of samples that should be transformed.
+
+        y: Array-like
+            The true labels.
+
+        Returns
+        -------
+        StandardScaler
+            self
+
+        """
+        X = np.asarray(X)
+        self.window.add_element(X)
+        return self
