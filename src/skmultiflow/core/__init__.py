e3c413de19f49f96e16c25e9d688052a246c4c3a
Mert OZER <mertozer94@gmail.com>
2020-01-14 00:46:09 +0100

    Add unit tests for generic wrapper forecasting class.

--- src/skmultiflow/core/__init__.py
+++ src/skmultiflow/core/__init__.py
@@ -5,12 +5,15 @@ The :mod:`skmultiflow.core` module covers core elements of `scikit-multiflow`.
 from .base import BaseSKMObject
 from .base import ClassifierMixin
 from .base import RegressorMixin
+from .base import ForecasterMixin
 from .base import MetaEstimatorMixin
 from .base import MultiOutputMixin
 from .base import clone
 from .base import is_classifier
 from .base import is_regressor
+from .base import is_forecaster
 from .pipeline import Pipeline
 
-__all__ = ["BaseSKMObject", "ClassifierMixin", "RegressorMixin", "MetaEstimatorMixin",
-           "MultiOutputMixin", "Pipeline", "clone", "is_classifier", "is_regressor"]
+__all__ = ["BaseSKMObject", "ClassifierMixin", "RegressorMixin",
+           "ForecasterMixin", "MetaEstimatorMixin", "MultiOutputMixin",
+           "Pipeline", "clone", "is_classifier", "is_regressor", "is_forecaster"]
