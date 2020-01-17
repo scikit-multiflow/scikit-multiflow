31fd5f4c34b8db8072b7aefbdacfa8e54c8f2424
is39 <imransalim08@gmail.com>
2020-01-17 14:53:03 +1300

    Add a working example to Naive Bayes and one that does not work for Half-Space Trees

--- src/skmultiflow/anomaly_detection/half_space_trees.py
+++ src/skmultiflow/anomaly_detection/half_space_trees.py
@@ -58,9 +58,42 @@ class HalfSpaceTrees(BaseSKMObject, ClassifierMixin):
 
     Examples
     --------
+    .. code-block:: python
 
-    >>> # Imports
-    >>> from skmultiflow.anomaly_detection import HalfSpaceTrees
+       # Imports
+       from skmultiflow.data import SEAGenerator
+       from skmultiflow.anomaly_detection import HalfSpaceTrees
+
+       # Setting up a data stream
+       stream = SEAGenerator(random_state=1)
+       stream.prepare_for_use()
+
+       # Setup Half-Space Trees estimator
+       half_space_trees = HalfSpaceTrees(n_features=1,
+                                         n_estimators=25,
+                                         window_size=250,
+                                         depth=15,
+                                         size_limit=50,
+                                         anomaly_threshold=0.5,
+                                         random_state=None)
+
+       # Setup variables to control loop and track performance
+       n_samples = 0
+       correct_cnt = 0
+       max_samples = 200
+
+       # Train the estimator(s) with the samples provided by the data stream
+       while n_samples < max_samples and stream.has_more_samples():
+           X, y = stream.next_sample()
+           y_pred = half_space_trees.predict(X) # Error is thrown here
+           if y[0] == y_pred[0]:
+               correct_cnt += 1
+           half_space_trees = half_space_trees.partial_fit(X, y)
+           n_samples += 1
+
+       # Display results
+       print('{} samples analyzed.'.format(n_samples))
+       print('Half-Space Trees accuracy: {}'.format(correct_cnt / n_samples))
     """
 
     def __init__(self,
