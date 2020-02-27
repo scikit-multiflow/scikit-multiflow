28bd1c2869e5d430d5ef380ca99132d312613a3e
jacobmontiel <17923265+jacobmontiel@users.noreply.github.com>
2020-02-27 14:19:25 +1300

    Improve test coverage

--- tests/meta/test_streaming_random_patches.py
+++ tests/meta/test_streaming_random_patches.py
@@ -3,6 +3,8 @@ from skmultiflow.meta import StreamingRandomPatchesClassifier
 
 import numpy as np
 
+import pytest
+
 
 def test_srp_randompatches():
 
@@ -12,11 +14,11 @@ def test_srp_randompatches():
                                                training_method='randompatches',
                                                random_state=1)
 
-    y_expected = np.asarray([0, 1, 0, 0, 0, 1, 1, 0, 0, 0,
-                             1, 1, 0, 1, 1, 1, 0, 1, 0, 0,
-                             0, 1, 0, 0, 0, 1, 0, 1, 0, 0,
-                             0, 0, 0, 1, 1, 0, 1, 0, 1, 1,
-                             0, 1, 1, 1, 1, 0, 0, 1, 0, 0], dtype=np.int)
+    y_expected = np.asarray([0, 0, 0, 0, 0, 1, 0, 0, 1, 1,
+                             0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+                             0, 0, 1, 0, 0, 0, 1, 1, 1, 0,
+                             1, 1, 0, 1, 0, 1, 0, 1, 1, 0,
+                             1, 1, 1, 0, 0, 0, 1, 0, 1, 0], dtype=np.int)
 
     run_prequential_supervised(stream, learner, max_samples=2000, n_wait=40, y_expected=y_expected)
 
@@ -29,11 +31,11 @@ def test_srp_randomsubspaces():
                                                training_method='randomsubspaces',
                                                random_state=1)
 
-    y_expected = np.asarray([0, 0, 1, 1, 0, 0, 0, 0, 0, 1,
-                             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             1, 1, 0, 0, 0, 0, 0, 1, 0, 1,
-                             1, 1, 1, 1, 1, 1, 0, 1, 0, 1,
-                             1, 0, 0, 1, 0, 1, 1, 1, 1, 0], dtype=np.int)
+    y_expected = np.asarray([0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                             0, 0, 0, 1, 0, 0, 0, 1, 0, 1,
+                             0, 0, 1, 0, 0, 1, 1, 0, 1, 0,
+                             1, 0, 0, 0, 0, 0, 1, 0, 0, 0,
+                             0, 0, 1, 0, 0, 0, 0, 0, 0, 0], dtype=np.int)
 
     run_prequential_supervised(stream, learner, max_samples=2000, n_wait=40, y_expected=y_expected)
 
@@ -45,11 +47,11 @@ def test_srp_resampling():
                                                training_method='resampling',
                                                random_state=1)
 
-    y_expected = np.asarray([1, 1, 0, 0, 0, 0, 0, 1, 0, 0,
-                             0, 0, 0, 1, 0, 0, 0, 0, 1, 0,
-                             0, 0, 1, 1, 0, 0, 1, 0, 0, 0,
-                             0, 1, 1, 1, 0, 0, 1, 1, 1, 1,
-                             0, 1, 1, 1, 0, 1, 1, 0, 1, 0], dtype=np.int)
+    y_expected = np.asarray([0, 0, 0, 0, 0, 1, 0, 0, 1, 1,
+                             0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+                             0, 0, 1, 0, 0, 0, 1, 1, 1, 0,
+                             1, 1, 0, 1, 1, 1, 0, 1, 1, 0,
+                             1, 1, 1, 0, 0, 0, 1, 0, 1, 0], dtype=np.int)
 
     run_prequential_supervised(stream, learner, max_samples=2000, n_wait=40, y_expected=y_expected)
 
@@ -82,8 +84,11 @@ def test_srp_coverage():
     learner.reset()
     assert learner.ensemble is None
 
+    with pytest.raises(ValueError):
+        _ = StreamingRandomPatchesClassifier(training_method='invalid')
 
-
+    with pytest.raises(ValueError):
+        _ = StreamingRandomPatchesClassifier(subspace_mode='invalid')
 
 
 def run_prequential_supervised(stream, learner, max_samples, n_wait, y_expected=None):
