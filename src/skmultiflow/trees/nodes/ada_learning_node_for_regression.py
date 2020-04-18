25a6fde9268872f7f4365ddd06c079d94d5c7abd
smastelini <saulomastelini@gmail.com>
2020-04-18 16:16:43 -0300

    Replay work done in #220

--- src/skmultiflow/trees/nodes/ada_learning_node_for_regression.py
+++ src/skmultiflow/trees/nodes/ada_learning_node_for_regression.py
@@ -30,6 +30,10 @@ class AdaLearningNodeForRegression(ActiveLearningNodePerceptron, AdaNode):
         self._error_change = False
         self._random_state = check_random_state(random_state)
 
+        # To normalize the observed errors in the [0, 1] range
+        self._min_error = float('Inf')
+        self._max_error = float('-Inf')
+
     # Override AdaNode
     def number_leaves(self):
         return 1
@@ -54,10 +58,8 @@ class AdaLearningNodeForRegression(ActiveLearningNodePerceptron, AdaNode):
 
         super().learn_from_instance(X, y, weight, rhat)
 
-        true_target = y
-        target_prediction = rhat.predict([X])[0]
-
-        normalized_error = rhat.get_normalized_error(target_prediction, true_target)
+        y_pred = rhat.predict([X])[0]
+        normalized_error = self.get_normalized_error(y, y_pred)
 
         if self._estimation_error_weight is None:
             self._estimation_error_weight = ADWIN()
@@ -86,3 +88,17 @@ class AdaLearningNodeForRegression(ActiveLearningNodePerceptron, AdaNode):
         if found_nodes is None:
             found_nodes = []
         found_nodes.append(FoundNode(self, parent, parent_branch))
+
+    def get_normalized_error(self, y, y_pred):
+        abs_error = abs(y - y_pred)
+
+        # Incremental maintenance of the normalization ranges
+        if abs_error < self._min_error:
+            self._min_error = abs_error
+        if abs_error > self._max_error:
+            self._max_error = abs_error
+
+        if self._min_error != self._max_error:
+            return (abs_error - self._min_error) / (self._max_error - self._min_error)
+        else:
+            return 0.0
