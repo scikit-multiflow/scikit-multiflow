517e92e5d904a8ab7057fc4647a7f5e751e4ba08
moravit <moravit@post.bgo.ac.il>
2020-06-01 12:38:56 +0300

    fixing imports

--- src/skmultiflow/trees/ifn/__init__.py
+++ src/skmultiflow/trees/ifn/__init__.py
@@ -1,9 +1,9 @@
-from . import utils
 from .meta_learning import MetaLearning
 from .data_processing_multi import DataProcessorMulti
 from .data_processing import DataProcessor
 from .ifn_network_multi import IfnNetworkMulti
 from .ifn_network import IfnNetwork
+from . import utils
 from .iolin import IncrementalOnlineNetwork
 
 
