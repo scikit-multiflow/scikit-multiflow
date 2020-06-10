4bfef8252282f44d8a07503cd9c1f33970511b28
moravit <moravit@post.bgo.ac.il>
2020-06-10 17:28:54 +0300

    removing multi

--- src/skmultiflow/trees/ifn/__init__.py
+++ src/skmultiflow/trees/ifn/__init__.py
@@ -4,11 +4,8 @@ for Incremental Fuzzy Network (IFN) methods.
 """
 
 from .data_processing import DataProcessor
-from .data_processing_multi import DataProcessorMulti
 from .ifn_network import IfnNetwork, HiddenLayer
-from .ifn_network_multi import IfnNetworkMulti
 from .iolin import IncrementalOnlineNetwork
 from .meta_learning import MetaLearning
 
-__all__ = ["DataProcessor", "DataProcessorMulti", "HiddenLayer", "IfnNetwork", "HiddenLayer",
-           "IfnNetworkMulti", "IncrementalOnlineNetwork", "MetaLearning"]
+__all__ = ["DataProcessor", "HiddenLayer", "IfnNetwork", "IncrementalOnlineNetwork", "MetaLearning"]
