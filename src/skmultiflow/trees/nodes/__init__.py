825b730179d358ef72a8542cfe0ca03d01780af9
smastelini <saulomastelini@gmail.com>
2020-07-11 15:56:44 -0300

    Simplify file names

--- src/skmultiflow/trees/nodes/__init__.py
+++ src/skmultiflow/trees/nodes/__init__.py
@@ -8,20 +8,19 @@ from .node import Node
 from .ada_node import AdaNode
 from .split_node import SplitNode
 from .learning_node import LearningNode
-from .hoeffding_tree_classifier_nodes import ActiveLearningNode, InactiveLearningNode, \
-    ActiveLearningNodeNB, ActiveLearningNodeNBAdaptive
-from .hoeffding_adaptive_tree_classifier_nodes import AdaSplitNode, AdaLearningNode
-from .arf_hoeffding_tree_classifier_nodes import RandomActiveLearningNode, \
-    RandomActiveLearningNodeNB, RandomActiveLearningNodeNBAdaptive
-from .extremely_fast_decision_tree_classifier_nodes import EFDTSplitNode, EFDTActiveLearningNode, \
-    EFDTInactiveLearningNode, EFDTActiveLearningNodeNB, EFDTActiveLearningNodeNBAdaptive
-from .lc_hoeffding_tree_classifier_nodes import LCActiveLearningNode, LCInactiveLearningNode, \
-    LCLearningNodeNB, LCLearningNodeNBA
-from .hoeffding_tree_regressor_nodes import ActiveLearningNodeForRegression, \
-    ActiveLearningNodePerceptron, InactiveLearningNodeForRegression, InactiveLearningNodePerceptron
+from .htc_nodes import ActiveLearningNode, InactiveLearningNode, ActiveLearningNodeNB, \
+    ActiveLearningNodeNBAdaptive
+from .hatc_nodes import AdaSplitNode, AdaLearningNode
+from .arf_htc_nodes import RandomActiveLearningNode, RandomActiveLearningNodeNB, \
+    RandomActiveLearningNodeNBAdaptive
+from .efdtc_nodes import EFDTSplitNode, EFDTActiveLearningNode, EFDTInactiveLearningNode, \
+    EFDTActiveLearningNodeNB, EFDTActiveLearningNodeNBAdaptive
+from .lc_htc_nodes import LCActiveLearningNode, LCInactiveLearningNode, LCLearningNodeNB, \
+    LCLearningNodeNBA
+from .htr_nodes import ActiveLearningNodeForRegression, ActiveLearningNodePerceptron, \
+    InactiveLearningNodeForRegression, InactiveLearningNodePerceptron
 from .arf_htr_nodes import RandomLearningNodeForRegression, RandomLearningNodePerceptron
-from .hoeffding_adaptive_tree_regressor_nodes import AdaSplitNodeForRegression, \
-    AdaLearningNodeForRegression
+from .hatr_nodes import AdaSplitNodeForRegression, AdaLearningNodeForRegression
 from .active_learning_node_for_regression_multi_target import \
     ActiveLearningNodeForRegressionMultiTarget
 from .active_learning_node_perceptron_multi_target import ActiveLearningNodePerceptronMultiTarget
