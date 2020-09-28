6ecfd622ec811c1c2fa5f301aed30e7b5c8f6cb0
Joze Rozanec <jmrozanec@gmail.com>
2020-09-28 13:03:59 +0200

    Update

--- src/skmultiflow/data/observer/buffer_result_observer.py
+++ src/skmultiflow/data/observer/buffer_result_observer.py
@@ -2,14 +2,12 @@ class BufferResultObserver():
     """ BufferResultObserver class.
     """
 
-    def __init__(self):
+    def __init__(self, metrics, reporter, preserve_data=False, buffer_size=20, ):
         """ BufferResultObserver class constructor."""
         self.y_pred_buffer = []
         self.y_true_buffer = []
 
-    # TODO: add policies, regarding we should preserve or discard data
-    # TODO: compute metrics and report them somewhere: write to file, print to println, use them to display some graph, etc
     def report(self, y_pred, y_true):
         self.y_pred_buffer = y_pred
         self.y_true_buffer = y_true
-        print("We just reported some results :)")
+        
